7b1992d30494dc5aad38fc2855dff0785d5f839a
songhyeongju <ardieyes@gmail.com>
2024-12-09 22:59:46 +0900

    다양한 타입들

--- src/advType.ts
+++ src/advType.ts
@@ -8,4 +8,15 @@
     [P in keyof T]?: T[P]; //for..in
   };
   type FruitsOptional = Optional<Fruits>;
+  // conditional
+  type Check<T> = T extends string ? boolean : null;
+  type TypeName<T> = T extends string
+    ? 'string'
+    : T extends number
+    ? 'function'
+    : 'object';
+  // read only
+  function fru(f: Readonly<Fruits>) {
+    // f.name='' 읽기 속성이라 재할당 불가능
+  }
 }
